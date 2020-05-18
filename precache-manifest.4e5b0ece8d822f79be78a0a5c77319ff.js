783d9b29b2952f2796faefa39ba75c158689e48d
Brian Fairbanks <brian.k.fairbanks@gmail.com>
2020-05-18 14:36:40 -0500

    Updates

---
+++ precache-manifest.4e5b0ece8d822f79be78a0a5c77319ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d204c83b7b7dc0c90153652231238018",
+    "url": "/DiceRollerz/index.html"
+  },
+  {
+    "revision": "c38fadd509576c598a02",
+    "url": "/DiceRollerz/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2e2354ddad07b74ddda6",
+    "url": "/DiceRollerz/static/js/2.bf6a35d7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/DiceRollerz/static/js/2.bf6a35d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c38fadd509576c598a02",
+    "url": "/DiceRollerz/static/js/main.1dd6d7ce.chunk.js"
+  },
+  {
+    "revision": "e529e80c19f1463c2f5e",
+    "url": "/DiceRollerz/static/js/runtime-main.67ea03ec.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/DiceRollerz/static/media/logo.5d5d9eef.svg"
+  }
+]);
