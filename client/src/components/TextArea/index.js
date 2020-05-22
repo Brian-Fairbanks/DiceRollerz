eba8a611d3388140f37d6bf72433cbf827bdaccf
Jonathan Andrews <ionathas78@hotmail.com>
2020-05-22 14:11:31 -0500

    TextareaWIcon added

--- client/src/components/TextArea/index.js
+++ client/src/components/TextArea/index.js
@@ -1,10 +1,29 @@
 import React from "react";
 
+/**
+ * Textarea component
+ * @param {Object} propz id, colSize, label, text
+ */
 export default function Textarea (propz) {
     const { id, colSize, text, label } = propz;
     return (
-        <div class={"input-field col " + colSize}>
-          <textarea id={id} class="materialize-textarea">{text}</textarea>
+        <div className={"input-field col " + colSize}>
+          <textarea id={id} className="materialize-textarea">{text}</textarea>
+          <label for={id}>{label}</label>
+        </div>
+    )
+}
+
+/**
+ * Textarea component with icon prefix
+ * @param {Object} propz id, colSize, icon, label, text
+ */
+export function TextareaWIcon (propz) {
+    const {id, colSize, text, label, icon } = propz;
+    return (
+        <div className={"input-field col " + colSize}>
+          <i className="material-icons prefix">{icon}</i>
+          <textarea id={id} className="materialize-textarea">{text}</textarea>
           <label for={id}>{label}</label>
         </div>
     )
