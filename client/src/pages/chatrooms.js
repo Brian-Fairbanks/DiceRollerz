0eba3ca579532a0c879c2711fdeef7c346b1461a
Brian Fairbanks <brian.k.fairbanks@gmail.com>
2020-05-22 12:26:32 -0500

    Using api to call chatrooms, and print list of all of them in client/chat

--- client/src/pages/chatrooms.js
+++ client/src/pages/chatrooms.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import API from "../utils/API";
 
-function chatrooms(){
+function Chatrooms(){
+
+  const [chatrooms, setChatrooms] = useState(["test"]);
+
+  useEffect(() => {
+    API.getChatrooms()
+    .then( data => {setChatrooms(data.data)})
+  }, [])
+
+
   return (
     <div>
       list the chatrooms here!
+      <br/>
+      {chatrooms.map(room => {
+        return (
+          <div>{room.name}</div>
+        )
+      })}
     </div>
   )
 }
 
-export default chatrooms;
+export default Chatrooms;
