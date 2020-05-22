7a00baa59adfee48ff6e1bc57711148136722ef3
Jonathan Andrews <ionathas78@hotmail.com>
2020-05-22 14:21:03 -0500

    InputWIcon added

--- client/src/components/Input/index.js
+++ client/src/components/Input/index.js
@@ -2,19 +2,39 @@ import React from "react";
 
 /**
  * Text Input control
- * @param {Object} propz id, colSize, placeholder, label, type ("text" by default)
+ * @param {Object} propz id, colSize, placeholder, label, type ("text" by default), inputClass ("validate" by default)
  */
 export default function Input(propz) {
-    const { id, colSize, placeholder, label, type } = propz
+    const { id, colSize, placeholder, label, type, inputClass } = propz
     return (
         <div className={"input-field col " + colSize}>
             <input 
                 id={id} 
-                className="validate" 
+                className={!inputClass ? "validate" : inputClass}
                 placeholder={placeholder} 
                 type={!type ? "text" : type} 
             />
             <label for={id}>{label}</label>
         </div>
     );
-  }
+}
+
+/**
+ * Text Input control
+ * @param {Object} propz id, colSize, placeholder, icon, label, type ("text" by default), inputClass ("validate" by default)
+ */
+export default function InputWIcon(propz) {
+    const { id, colSize, placeholder, label, type, inputClass, icon } = propz
+    return (
+        <div className={"input-field col " + colSize}>
+            <i class="material-icons prefix">{icon}</i>
+            <input 
+                id={id} 
+                className={!inputClass ? "validate" : inputClass}
+                placeholder={placeholder} 
+                type={!type ? "text" : type} 
+            />
+            <label for={id}>{label}</label>
+        </div>
+    );
+}
