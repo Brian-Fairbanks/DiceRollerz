968769c7666e2081892aaae1010eb97f867d2259
Brian Fairbanks <brian.k.fairbanks@gmail.com>
2020-05-23 10:28:44 -0500

    Installed socket.io-client - set up socketMsg in apis to emit to server. - server detecting msg sent, and displaying in console.

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,4 +1,7 @@
 import axios from "axios";
+// set up socket info 
+import openSocket from 'socket.io-client';
+const socket = openSocket('http://localhost:3001');
 
 export default {
   getChatrooms: function(query) {
@@ -14,6 +17,14 @@ export default {
   },
 
   sendPost: function(post){
+    this.socketMsg();
     return axios.post("/api/post", post);
   },
+
+
+
+  socketMsg(){
+    console.log("Should emit to socket")
+    socket.emit('chatMessage', ("Testing"));
+  }
 };
