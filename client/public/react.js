fbb249a9c1ebb6dedafbbafcc69c0c9605155a80
Lee Ann Norman <leeann.norm@gmail.com>
2020-05-25 19:35:25 -0500

    first commit (again--see other commits)

---
+++ client/public/react.js
@@ -0,0 +1,6 @@
+let navBar = $('<div>').addClass('bg-dark text-light p-5 mt-0 text-center display-4').text('DICE ROLLERZ');
+
+let footBar = $('<div>').addClass('bg-danger text-light p')
+
+let root = $('#root');
+root.append(navBar);
