cbb7215e5cf2aed4713ec67a39a939dbc57339d3
Lee Ann Norman <leeann.norm@gmail.com>
2020-05-26 00:32:15 -0500

    materialize is a very difficult framework. Set up basic components for material design instead. Also going to looks at sass. Navbar, Container, Footer, index.js, and app.js as well as css files added

--- client/src/App.js
+++ client/src/App.js
@@ -1,79 +1,76 @@
-import React, {useState, useEffect} from 'react';
-import {Switch, BrowserRouter, Route} from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { Switch, BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 import { Avatar, AvatarWPic } from './components/Avatar';
-import Chat from "./pages/chatrooms";
-import Profile from "./pages/profile";
+import Chat from './pages/chatrooms';
+import Profile from './pages/profile';
 import API from './utils/API';
-import UserContext from "./utils/userContext";
-import NavBar from "./components/Navbar";
+import UserContext from './utils/userContext';
+import NavBar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+// import AppBar from '@material-ui/core/AppBar';
+// import Toolbar from '@material-ui/core/Toolbar';
+// import TypoGraphy from '@material-ui/core/Typography';
 
-function App() {
+function App () {
+  return (
+    /* <AppBar>
+          <Toolbar>
+            <TypoGraphy variant="title" color="inherit">
+              My header
+            </TypoGraphy>
+          </Toolbar>
+        </AppBar>
+        <br></br> */
+    <div className='App'>
+      <NavBar />
+      <Footer />
+    </div>
+  )
+}
 
-  const [user, setUser ] = useState({_id:"", firstName:"", lastName:"", userName:"", email:""})
+function Appfxn () {
+  const [user, setUser] = useState({
+    _id: '',
+    firstName: '',
+    lastName: '',
+    userName: '',
+    email: ''
+  })
   // handle setting user after login or signup!
-  
+
   // this here is only temporary !!! -------------
   useEffect(() => {
-    API.getUsers()
-    .then( data => {setUser(data.data[0])})
+    API.getUsers().then((data) => {
+      setUser(data.data[0])
+    })
   }, [])
   //  this here is only temporary !!! -------------
 
   return (
-    <UserContext.Provider value={{ user}}>
-
-      <div className="App">
-
-        <div className="red darken-4">
+    <UserContext.Provider value={{ user }}>
+      <div className='App'>
+        <div className='red darken-4'>
           <h1>Header - Dice Rollers FTW!</h1>
           <div>Hello, {user.userName}</div>
         </div>
 
-
         <BrowserRouter>
           <Switch>
-            <Route exact path={["/","/login"]}>
+            <Route exact path={['/', '/login']}>
               <div>Must add login page here!</div>
             </Route>
-            <Route exact path={"/chat"}>
-              <Chat/>
+            <Route exact path='/chat'>
+              <Chat />
             </Route>
-            <Route exact path={"/profile"}>
-              <Profile/>
+            <Route exact path='/profile'>
+              <Profile />
             </Route>
-            
           </Switch>
         </BrowserRouter>
       </div>
     </UserContext.Provider>
-  );
+  )
 }
 
-import React, { Component } from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar'
-import TypoGraphy from '@material-ui/core/Typography'
-
-
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <AppBar color="danger" position="static">
-          <Toolbar>
-            <TypoGraphy variant="title"
-              color="inherit"
-            >
-              My header
-           </TypoGraphy>
-          </Toolbar>
-        </AppBar>
-
-      </div>
-    );
-  }
-}
-
-
-export default App;
+export default App
--- client/src/components/Navbar/navbar.js
+++ client/src/components/Navbar/navbar.js
@@ -1,42 +1,128 @@
-import React from "react";
+import React from 'react'
+import './Navbar.css'
 
+export default function NavBar () {
+  return (
+    <div className='nav'>
+      <h1>Test</h1>
+    </div>
+  )
+};
 
-import React from 'react';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import TypoGraphy from '@material-ui/core/Typography'
+// import React, { Component } from 'react'
+// import { Button, Card, Row, Col } from 'react-materialize';
 
+// class NavBar extends Component {
+//     render()
+//     return {
+//         <div className = "App">
+//             <header className="App-header">
+//             <img src={logo} className="App-logo" alt="logo" />
 
-export default function NavBar(props) {
+//     </header>
+//     </ div >
+//     }
+// }
 
-    return (
-        <List component="nav">
-            <ListItem component="div">
-                <ListItemText inset>
-                    <TypoGraphy color="inherit" variant="title">
-                        Home
-               </TypoGraphy>
-                </ListItemText>
+// import React, {
+//   Fragment,
+//   Children,
+//   cloneElement,
+//   useRef,
+//   useEffect
+// } from 'react';
+// import PropTypes from 'prop-types';
+// import cx from 'classnames';
+// import Icon from './Icon';
+// import SearchForm from './SearchForm';
 
+// const Navbar = ({
+//   id,
+//   children,
+//   brand,
+//   className,
+//   extendWith,
+//   fixed,
+//   alignLinks,
+//   centerLogo,
+//   search,
+//   menuIcon,
+//   sidenav,
+//   options,
+//   centerChildren,
+//   ...props
+// }) => {
+//   const _sidenav = useRef(null);
 
-                <ListItemText inset>
-                    <TypoGraphy color="inherit" variant="title">
-                        Posts
-               </TypoGraphy>
-                </ListItemText>
+//   useEffect(() => {
+//     const instance = M.Sidenav.init(_sidenav.current, options);
 
+//     return () => {
+//       instance && instance.destroy();
+//     };
+//   }, [options, children]);
 
-                <ListItemText inset>
-                    <TypoGraphy color="inherit" variant="title">
-                        Contact
-               </TypoGraphy>
-                </ListItemText>
-            </ListItem >
+//   const brandClasses = cx({
+//     'brand-logo': true,
+//     right: alignLinks === 'left',
+//     center: centerLogo
+//   });
 
-        </List>
-    )
-}
+//   const navCSS = cx({ 'nav-extended': extendWith }, className);
 
+//   const navMobileCSS = cx('hide-on-med-and-down', [alignLinks]);
 
-export default NavBar;
+//   const links = Children.map(children, (link, index) => (
+//     <li key={index}>{link}</li>
+//   ));
+
+//   const sidenavLinks = sidenav
+//     ? sidenav
+//     : Children.map(children, (link, index) => {
+//         const clonedLink =
+//           link && link.props && link.props.id
+//             ? cloneElement(link, {
+//                 ...link.props,
+//                 id: `sidenav-${link.props.id}`
+//               })
+//             : link;
+//         return <li key={index}>{clonedLink}</li>;
+//       });
+
+//   let navbar = (
+//     <nav className={navCSS} {...props}>
+//       <div className={cx({ container: centerChildren }, 'nav-wrapper')}>
+//         {search ? (
+//           <SearchForm />
+//         ) : (
+//           <Fragment>
+//             {brand &&
+//               cloneElement(brand, {
+//                 className: cx(brand.props.className, brandClasses)
+//               })}
+//             <a href="#!" data-target={id} className="sidenav-trigger">
+//               {menuIcon}
+//             </a>
+//             <ul className={navMobileCSS}>{links}</ul>
+//           </Fragment>
+//         )}
+//       </div>
+//       {extendWith && <div className="nav-content">{extendWith}</div>}
+//     </nav>
+//   );
+
+//   if (fixed) {
+//     navbar = <div className="navbar-fixed">{navbar}</div>;
+//   }
+
+//   return (
+//     <Fragment>
+//       {navbar}
+
+//       <ul id={id} className={cx('sidenav', [alignLinks])} ref={_sidenav}>
+//         {sidenavLinks}
+//       </ul>
+//     </Fragment>
+//   );
+// }
+// export default Navbar;
