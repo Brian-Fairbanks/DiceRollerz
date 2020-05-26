7fc119af63153616b0cea447823d8209c8025258
Jonathan Andrews <ionathas78@hotmail.com>
2020-05-26 13:44:54 -0500

    abandoned changes

--- client/src/pages/profile.js
+++ client/src/pages/profile.js
@@ -4,22 +4,7 @@ import { Avatar, AvatarWPic } from '../components/Avatar';
 import { Input, InputWIcon } from '../components/Input';
 import { Textarea, TextareaWIcon } from '../components/TextArea';
 
-function Profile(userId) {
-  const [user, setUser ] = useState({_id:"", firstName:"", lastName:"", userName:"", email:"", description: ""})
-  // handle setting user after login or signup!
-  
-  // this here is only temporary !!! -------------
-  useEffect(() => {
-    API.getUser(userId)
-    .then(result => {
-      console.log(result);
-      setUser(result.data);
-    })
-    .catch(err => { console.error(err) });
-  }, [])
-  //  this here is only temporary !!! -------------
-
-
+function App() {
   return (
     <div className="App">
       <div className="row">
@@ -29,11 +14,12 @@ function Profile(userId) {
             <Avatar>
                 <AvatarWPic
                     imagePath="https://static01.nyt.com/images/2018/05/15/arts/01hal-voice1/merlin_135847308_098289a6-90ee-461b-88e2-20920469f96a-superJumbo.jpg?quality=90&auto=webp"
-                    imageHeight="100px"
+                    imageHeight="50px"
                     altText="Red Dot"
                     title="HAL 9000"
                     text={["I'm sorry, Dave", "I'm afraid I can't do that."]}
                     href="#"
+                    key="1"
                 />
             </Avatar>
               
@@ -44,7 +30,6 @@ function Profile(userId) {
                 label="User Name"
                 placeholder="User Name"
                 type="text"
-                value={user.userName}
               />
               <InputWIcon
                 id="first-name"
@@ -53,7 +38,6 @@ function Profile(userId) {
                 label="First Name"
                 placeholder="First Name"
                 type="text"
-                value={user.firstName}
               />
               <InputWIcon
                 id="last-name"
@@ -62,7 +46,6 @@ function Profile(userId) {
                 label="Last Name"
                 placeholder="Last Name"
                 type="text"
-                value={user.lastName}
               />
               <InputWIcon
                 id="email"
@@ -71,16 +54,18 @@ function Profile(userId) {
                 label="Email"
                 placeholder="Email"
                 type="text"
-                value={user.email}
               />
               <TextareaWIcon
                 id="description"
                 name="desc"
                 icon=""
                 label="About Me"
-                value={user.description}
               />
               
+
+
+
+
           </form>
         </div>
       </div>
@@ -88,4 +73,4 @@ function Profile(userId) {
   );
 }
 
-export default Profile;
+export default App;
