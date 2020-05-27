69cb4a8e742b54c45b5288b17f5e7167a5debd83
Brian Fairbanks <brian.k.fairbanks@gmail.com>
2020-05-26 19:45:36 -0500

    further Styling to messages

--- client/src/components/Message/index.js
+++ client/src/components/Message/index.js
@@ -2,9 +2,9 @@ import React from "react";
 
 function Message({body, deleted, updated, sender, yours, id,  getMsg, time}){
   return(
-    <div className={"col s12"} onClick={() => getMsg(body, id)}>
+    <div className={"col s12 white-text"} onClick={() => getMsg(body, id)}>
       <div>{time}</div>
-      <div className={`col s10 push-s1 ${yours?"right-align red":"left-align teal"}`}>
+      <div className={`message col s10 push-s1 left-align ${yours?"red":"teal"}`}>
         {
           deleted? "Message has been deleted":
             `${body} ${updated?"  (Updated)":""}`}
