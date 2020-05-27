3e5641f742f36104c076de91aa84fc7dabfeb00c
Brian Fairbanks <brian.k.fairbanks@gmail.com>
2020-05-27 09:41:45 -0500

    deleted messages now have no textm just a lower opacity bubble

--- client/src/components/Message/index.js
+++ client/src/components/Message/index.js
@@ -4,9 +4,9 @@ function Message({body, deleted, updated, sender, yours, id,  getMsg, time}){
   return(
     <div className={"col s12 white-text"} onClick={() => getMsg(body, id)}>
       <div>{time}</div>
-      <div className={`message col s10 push-s1 left-align ${yours?"red":"teal"}`}>
+      <div className={`message col s10 push-s1 left-align ${yours?"red":"teal"} ${deleted? "fade":""}`}>
         {
-          deleted? "Message has been deleted":
+          deleted? "":
             `${body} ${updated?"  (Updated)":""}`}
             {/*yours? <i className="far fa-edit"></i> :""*/}
       </div>
