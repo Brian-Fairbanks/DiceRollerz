cda514c899e91ccd188a5198fcdb8e8451370435
Sergio Bracamontes <sbracamontes458@cardinal.wju.edu>
2020-05-28 15:37:59 -0500

    making merges

--- client/src/pages/SignUp/signup.js
+++ client/src/pages/SignUp/signup.js
@@ -41,8 +41,8 @@ return(
       <div class="card blue-grey darken-1">
         <div class="card-content white-text">
           <input placeholder="Username" name="username" onChange={handleInputChange} type="text" className="white-text"></input>
-          <input placeholder="Password" name="password" onChange={handleInputChange} type="text" className="white-text"></input>
-          <input placeholder="Confirm Password" onChange={handleInputChange} name="password2" type="text" className="white-text"></input>
+          <input placeholder="Password" name="password" onChange={handleInputChange} type="password" className="white-text"></input>
+          <input placeholder="Confirm Password" onChange={handleInputChange} name="password2" type="password" className="white-text"></input>
           <input placeholder="Email" name="email" onChange={handleInputChange} type="email" className="white-text"></input>
           <input placeholder="First Name" onChange={handleInputChange} name="firstName" type="text" className="white-text"></input>
           <input placeholder="Last Name" onChange={handleInputChange} name="lastName" type="text" className="white-text"></input>
