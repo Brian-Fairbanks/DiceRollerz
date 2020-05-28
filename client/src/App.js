b805610f0b170d7a529b8dec3aacc7f22ea6fe15
Jason Strouphauer <jdstroup10@gmail.com>
2020-05-28 15:05:08 -0500

    Committing to create seperate branch

--- client/src/App.js
+++ client/src/App.js
@@ -11,42 +11,18 @@ import Landing from "./pages/landing"
 //import Login from "./pages/Login/login";
 import SignUp from './pages/SignUp/signup';
 import Login from './pages/Login/logintest.js';
-import { Provider } from "react-redux";
 import store from "./store";
 import { Provider } from "react-redux";
-import store from "./store";
-import jwt_decode from "jwt-decode";
-import setAuthToken from "./utils/setAuthToken";
-import { setCurrentUser, logoutUser } from "./actions/authActions";
-import PrivateRoute from "./components/Private-Route/PrivateRoute";
-import Dashboard from "./components/Dashboard/Dashboard";
-
-// Check for token to keep user logged in
-if (localStorage.jwtToken) {
-  console.log("checking for token")
-  // Set auth token header auth
-  const token = localStorage.jwtToken;
-  setAuthToken(token);
-  // Decode token and get user info and exp
-  const decoded = jwt_decode(token);
-  // Set user and isAuthenticated
-  store.dispatch(setCurrentUser(decoded));
-// Check for expired token
-  const currentTime = Date.now() / 1000; // to get in milliseconds
-  if (decoded.exp < currentTime) {
-    // Logout user
-    store.dispatch(logoutUser());
-    // Redirect to login
-    window.location.href = "./login";
-  }
-}
-
 import jwt_decode from "jwt-decode";
 import setAuthToken from "./utils/setAuthToken";
 import { setCurrentUser, logoutUser } from "./actions/authActions";
 
 import PrivateRoute from "./components/private-route/PrivateRoute";
 import Dashboard from "./components/dashboard/Dashboard";
+
+
+
+
 
 // Check for token to keep user logged in
 if (localStorage.jwtToken) {
