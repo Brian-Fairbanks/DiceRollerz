cfa5da5c5fd8d79f393484ff4e14b65a8591b67f
Brian Fairbanks <brian.k.fairbanks@gmail.com>
2020-05-29 16:51:45 -0500

    Removed un-needed dependencies from APP.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,13 @@ import React, { useState, useEffect } from 'react';
 import API from './utils/API';
 import UserContext from './utils/userContext';
 
-import store from "./store";
-import { Provider } from "react-redux";
-
 //Routing Dependencies
 import { Switch, BrowserRouter, Route } from 'react-router-dom';
-import PrivateRoute from "./components/Private-Route";
+//import PrivateRoute from "./components/Private-Route";
 
 // Authentication Dependencies
 import jwt_decode from "jwt-decode";
 import setAuthToken from "./utils/setAuthToken";
-import { setCurrentUser, logoutUser } from "./actions/authActions";
 
 // Styling Dependencies
 import M from  'materialize-css/dist/js/materialize.min.js';
