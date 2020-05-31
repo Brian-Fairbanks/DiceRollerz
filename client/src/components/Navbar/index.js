3fe748c1abe44a53cdfaab3a30d1e07a48a5f5a0
Brian Fairbanks <brian.k.fairbanks@gmail.com>
2020-05-31 00:34:18 -0500

    removed bypass on signin validation

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -22,7 +22,6 @@ function Nav() {
       email: ''
     });
     setToken("NotSet");
-
   }
 
 
--- validation/login.js
+++ validation/login.js
@@ -3,10 +3,6 @@ const isEmpty = require("is-empty");
 module.exports = function validateLoginInput(data) {
   let errors = {};
 // Convert empty fields to an empty string so we can use validator functions
-  return {
-    errors: [],
-    isValid: true
-  }
   data.username = !isEmpty(data.username) ? data.username : "";
   data.password = !isEmpty(data.password) ? data.password : "";
 // Email checks
