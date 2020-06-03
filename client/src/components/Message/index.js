be793d8fa6d37ffbf8c13bd62e02d24772356eb7
Brian Fairbanks <brian.k.fairbanks@gmail.com>
2020-06-03 16:03:51 -0500

    Styled messages                                                                                - grouped user messages when spamming                                                                          - moved timestamp                                                                                              - moved avatar image

--- client/src/components/Message/index.js
+++ client/src/components/Message/index.js
@@ -3,7 +3,7 @@ import moment from 'moment'
 import "./styles.css";
 
 
-function Message({ members, body, deleted, updated, sender, yours, id, getMsg, time }) {
+function Message({ members, body, deleted, updated, sender, yours, id, getMsg, time, toGroup}) {
 
   const thisUser = members ? members.find(member => member.user === sender):"";
   const userImage = thisUser?thisUser.image : "";
@@ -11,20 +11,19 @@ function Message({ members, body, deleted, updated, sender, yours, id, getMsg, t
   return (
     <div className={"col s12 white-text"} onClick={() => getMsg(body, id, sender)}>
       <div className ="col s11 offset-s1 left-align valign-wrapper">
-        <div className="user-name">{userName}</div>
-        <div className="timestamp">{moment(time).format('h:mm:ss a')}</div>
+        {toGroup?"":<div className="user-name"> {userName} </div>}
       </div>
-      <div className={`col s1 right-align valign-wrapper`}>
-      {userImage?
-          <img src={userImage} alt="avatar image" className="circle right" height="32" width="32"></img> 
-          :
-          ""
-        }
+      <div className="col s1 avatarWrapper">
+      {userImage && !toGroup?
+          <img src={userImage} alt={userName+"-avatar"} className="circle avatar avatar-glow"></img> 
+        :
+        ""
+      }
       </div>
-      <div className={`message col s10 left-align ${yours ? "red" : "teal"} ${deleted ? "fade" : ""}`}>
+      <div className={`message col s10 left-align ${toGroup?" grouped ":""}${yours ? "red" : "teal"} ${deleted ? "fade" : ""}`}>
         {
-          deleted ? "" :
-            <div>{body} {updated ? <span className="edit">(edited)</span> : ""} </div>}
+          deleted ? "":
+            <div>{time} {body} {updated ? <span className="edit">(edited)</span> : ""} </div> }
         {/*yours? <i className="far fa-edit"></i> :""*/}
       </div>
     </div>
