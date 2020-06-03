3fa4367391b8be5d3dcf7ba764e164a4c3595635
Brian Fairbanks <brian.k.fairbanks@gmail.com>
2020-06-03 14:27:47 -0500

    Added username, and formatted time stamp along with it - changed formating for edited indicator

--- client/src/components/Message/index.js
+++ client/src/components/Message/index.js
@@ -1,18 +1,22 @@
 import React from "react";
 import moment from 'moment'
+import "./styles.css";
 
 
 function Message({ members, body, deleted, updated, sender, yours, id, getMsg, time }) {
 
-  const userImage = members ? members.find(member => member.user === sender).image : "";
+  const thisUser = members ? members.find(member => member.user === sender):"";
+  const userImage = thisUser?thisUser.image : "";
+  const userName = thisUser?thisUser.username : "";
   return (
     <div className={"col s12 white-text"} onClick={() => getMsg(body, id, sender)}>
-      <div>
-        {moment(time).format('h:mm:ss a')}
+      <div className ="col s11 offset-s1 left-align valign-wrapper">
+        <div className="user-name">{userName}</div>
+        <div className="timestamp">{moment(time).format('h:mm:ss a')}</div>
       </div>
-      <div className={`col s1 center-align valign-wrapper`}>
+      <div className={`col s1 right-align valign-wrapper`}>
       {userImage?
-          <img src={userImage} alt="avatar image" className="circle" height="32" width="32"></img> 
+          <img src={userImage} alt="avatar image" className="circle right" height="32" width="32"></img> 
           :
           ""
         }
@@ -20,7 +24,7 @@ function Message({ members, body, deleted, updated, sender, yours, id, getMsg, t
       <div className={`message col s10 left-align ${yours ? "red" : "teal"} ${deleted ? "fade" : ""}`}>
         {
           deleted ? "" :
-            `${body} ${updated ? "  (Updated)" : ""}`}
+            <div>{body} {updated ? <span className="edit">(edited)</span> : ""} </div>}
         {/*yours? <i className="far fa-edit"></i> :""*/}
       </div>
     </div>
