74791d3db44e0e7de336abdb26e69e5a47c379f1
Brian Fairbanks <brian.k.fairbanks@gmail.com>
2020-06-04 12:59:30 -0500

    Checking each post for slash commands - added default return that command is invalid - added case for /roll

--- controllers/postController.js
+++ controllers/postController.js
@@ -15,18 +15,48 @@ module.exports = {
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
-  create: function(req, res) {
+  create: async function(req, res) {
     console.log("Creating Post")
     console.log(req.body);
+    req.body.body = await checkCommands(req.body.body);
     db.Post
       .create(req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
-  update: function(req, res) {
+  update: async function(req, res) {
+    req.body.body = await checkCommands(req.body.body);
     db.Post
       .findOneAndUpdate({ _id: req.params.id }, { ...req.body,  updated:true})
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   }
 };
+
+
+/* Helper Functions
+================================= */
+
+async function checkCommands(msg){
+  // return msg as it is
+  if(msg[0] !== "/"){
+    return msg;
+  }
+  args = msg.split(" ");
+
+  const command = args[0].substring(1).toLowerCase();
+
+  switch(command){
+    case "roll":
+      return msg+` - ${await roll(args)}`;
+      break
+    default:
+      return msg+` - ${command} is not a valid /command`;
+  }
+}
+
+// Roll Command
+async function roll(args){
+  return "Rolling"
+}
+
