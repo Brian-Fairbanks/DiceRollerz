67b474ee6f47bed779c6401eb967026a1e19966e
Brian Fairbanks <brian.k.fairbanks@gmail.com>
2020-06-09 11:37:35 -0500

    Rending nav links only if signed in - otherwise, creating spans with the same icons greyed out

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,21 +1,34 @@
-import React, { useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 import Logo from "./DRZ.png";
 import userContext from "../../utils/userContext";
+import block from "material-ui/svg-icons/content/block";
 
 
 function Nav(props) {
-  const {M} = props;
-  document.addEventListener('DOMContentLoaded', function() {
+  const { setUser, setToken, token } = useContext(userContext);
+
+  const { M } = props;
+
+  document.addEventListener('DOMContentLoaded', function () {
     let sidenav = document.querySelector('.sidenav.fullnav');
     console.log(sidenav);
     M.Sidenav.init(sidenav, {});
   })
 
+  // User Signed In?
+  const [validated, setValidated] = useState(false);
+
+  useEffect(() => {
+    if (token && token !== "NotSet") {
+      setValidated(true);
+    }
+  }, [token])
+
+
   // Log Out Functionality
   // ========================
-  const { setUser, setToken } = useContext(userContext);
 
   function logOut() {
     console.log("Logging out!");
@@ -43,9 +56,20 @@ function Nav(props) {
         <div className="hide-on-small-only">
           <ul className="right">
             <li><Link to="/" title="Home"><i className="material-icons">home</i></Link></li>
-            <li><Link to="/profile" title="My Profile"><i className="small material-icons">person</i></Link></li>
-            <li><Link to="/chat" title="Chat Rooms"><i className="small material-icons">chat_bubble</i></Link></li>
-            <li><a onClick={() => { logOut() }}><i className="small material-icons">phonelink_erase</i></a></li>
+            {validated ? (
+              <>
+                <li><Link to="/profile" title="My Profile"><i className="small material-icons">person</i></Link></li>
+                <li><Link to="/chat" title="Chat Rooms"><i className="small material-icons">chat_bubble</i></Link></li>
+                <li><a onClick={() => { logOut() }}><i className="small material-icons">phonelink_erase</i></a></li>
+              </>
+            ) : 
+            (
+              <>
+                <li><i className="small material-icons disabled-nav-icon">person</i></li>
+                <li><i className="small material-icons disabled-nav-icon">chat_bubble</i></li>
+                <li><i className="small material-icons disabled-nav-icon">phonelink_erase</i></li>
+              </>
+            )}
           </ul>
         </div>
 
@@ -53,9 +77,19 @@ function Nav(props) {
 
       <ul id="nav-mobile" className="sidenav fullnav blue-grey darken-3">
         <li><Link to="/" className="amber-text"><i className="material-icons" style={{ color: "#ffc107" }}>home</i>Home</Link></li>
-        <li><Link to="/profile" className="amber-text"><i className="small material-icons" style={{ color: "#ffc107" }}>person</i> My Profile</Link></li>
-        <li><Link to="/chat" className="amber-text"><i className="small material-icons" style={{ color: "#ffc107" }}>chat_bubble</i> Chat Rooms</Link></li>
-        <li><a onClick={() => { logOut() }} className="amber-text"><i className="small material-icons" style={{ color: "#ffc107" }}>phonelink_erase</i> Log Out</a></li>
+        {validated ? (
+          <>
+            <li><Link to="/profile" className="amber-text"><i className="small material-icons" style={{ color: "#ffc107" }}>person</i> My Profile</Link></li>
+            <li><Link to="/chat" className="amber-text"><i className="small material-icons" style={{ color: "#ffc107" }}>chat_bubble</i> Chat Rooms</Link></li>
+            <li><a onClick={() => { logOut() }} className="amber-text"><i className="small material-icons" style={{ color: "#ffc107" }}>phonelink_erase</i> Log Out</a></li>
+          </>
+        ) : (
+          <>
+          <li><span className="amber-text flex disabled"><i className="small material-icons disabled-icon">person</i> My Profile</span></li>
+            <li><span className="amber-text flex disabled"><i className="small material-icons disabled-icon">chat_bubble</i> Chat Rooms</span></li>
+            <li><span className="amber-text flex disabled"><i className="small material-icons disabled-icon">phonelink_erase</i> Log Out</span></li>
+          </>
+        )}
       </ul>
 
       {/* <ul id="chat-nav" className="sidenav chatnav blue-grey darken-3">
